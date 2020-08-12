e3a938b0fea7e5594d2313eef6f31d09bc44cb1b
sachinthazoysa <55564245+sachinthazoysa@users.noreply.github.com>
2020-08-12 22:29:24 +0530

    add batch form done

--- src/TimeTableGeneratorDesktopApp/StudentBatches/studentBatchesController.java
+++ src/TimeTableGeneratorDesktopApp/StudentBatches/studentBatchesController.java
@@ -1,5 +1,7 @@
 package TimeTableGeneratorDesktopApp.StudentBatches;
 
+import TimeTableGeneratorDesktopApp.FxmlLoader;
+import TimeTableGeneratorDesktopApp.Main;
 import com.sun.glass.ui.EventLoop;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -8,15 +10,21 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.awt.*;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class studentBatchesController implements Initializable {
     @FXML
     private Button addBathcBtn;
+
+    @FXML
+    private Pane studentsPane;
 
     @FXML
     void ActionEventAddBatchBtn(ActionEvent event) {
@@ -26,11 +34,26 @@ public class studentBatchesController implements Initializable {
             Stage stage = new Stage();
 
             stage.setTitle("Add batch");
+            stage.initStyle(StageStyle.TRANSPARENT);
+
             stage.setScene(new Scene(root1));
             stage.show();
+
         }catch (Exception e){
             System.out.println("can't load new window");
         }
+    }
+
+    public void subGroupButtonAction(javafx.event.ActionEvent actionEvent) throws IOException {
+
+        URL fileURL = Main.class.getResource("/TimeTableGeneratorDesktopApp/StudentBatches/SubGroups/subGroups.fxml");
+        if (fileURL == null) {
+            throw new java.io.FileNotFoundException("FXML File can not be found");
+        }else{
+            Pane newPane = new FXMLLoader().load(fileURL);
+            studentsPane.getChildren().addAll(newPane);
+        }
+
     }
 
     @Override
