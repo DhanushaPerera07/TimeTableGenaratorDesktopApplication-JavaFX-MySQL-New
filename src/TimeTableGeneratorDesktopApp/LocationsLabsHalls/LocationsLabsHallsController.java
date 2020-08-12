8be1d2cd3eaa96b77c91de86b8409a4e20437f99
DhanushaPerera07 <buddhika.dhanusha@gmail.com>
2020-08-13 01:02:43 +0530

    Commit 6 - by Dhanusha
    * Worked on LocationsLabsHalls.fxml UI.. Just the UI
    * LocationsLabsHallsController.java created too..

---
+++ src/TimeTableGeneratorDesktopApp/LocationsLabsHalls/LocationsLabsHallsController.java
@@ -0,0 +1,13 @@
+package TimeTableGeneratorDesktopApp.LocationsLabsHalls;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LocationsLabsHallsController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
