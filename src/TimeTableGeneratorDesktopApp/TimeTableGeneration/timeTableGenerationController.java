ccd50bd449acd084ae7ba20bd6d17f5860f2b05e
janakachinthana <janakachinthana1@gmail.com>
2020-08-12 17:35:04 +0530

    my first commit to test

---
+++ src/TimeTableGeneratorDesktopApp/TimeTableGeneration/timeTableGenerationController.java
@@ -0,0 +1,13 @@
+package TimeTableGeneratorDesktopApp.TimeTableGeneration;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class timeTableGenerationController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
