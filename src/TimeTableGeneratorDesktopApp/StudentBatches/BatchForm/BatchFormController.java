57e3760190e8fa650eeefa65d498a0673d4200eb
sachinthazoysa <55564245+sachinthazoysa@users.noreply.github.com>
2020-08-13 17:15:03 +0530

    Done upto retrieving batches from the DB

--- src/TimeTableGeneratorDesktopApp/StudentBatches/BatchForm/BatchFormController.java
+++ src/TimeTableGeneratorDesktopApp/StudentBatches/BatchForm/BatchFormController.java
@@ -1,5 +1,7 @@
 package TimeTableGeneratorDesktopApp.StudentBatches.BatchForm;
 
+import TimeTableGeneratorDesktopApp.StudentBatches.studentBatchesController;
+import com.sun.javafx.scene.control.InputField;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -7,12 +9,27 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
+import java.awt.*;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 import java.util.ResourceBundle;
 
+
+
 public class BatchFormController implements Initializable {
+    public static String year = "";
+    public static String semester = "";
+    public static String intake = "";
+    public static String faculty = "";
+    public static String programme = "";
+    public static String center = "";
+    public static Integer noOfStudents = 0;
+
 
     @FXML
     private ComboBox<String> comboBoxYear;
@@ -33,57 +50,115 @@ public class BatchFormController implements Initializable {
     private ComboBox<String> comBoxCen;
 
 
+    @FXML
+    private TextField tfNoOfStd;
 
     @FXML
     public Button submitAddBatch;
 
+
+    TimeTableGeneratorDesktopApp.StudentBatches.studentBatchesController studentBatchesController = new studentBatchesController();
+
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
-        comboBoxYear.getItems().removeAll(comboBoxYear.getItems());
-        comboBoxYear.getItems().addAll(
-                "Year 1 ", "Year 2", "Year 3", "Year 4"
-        );
+        if(TimeTableGeneratorDesktopApp.StudentBatches.studentBatchesController.year.equals("")){
+            comboBoxYear.getItems().removeAll(comboBoxYear.getItems());
+            comboBoxYear.setPromptText("Select");
+            comboBoxYear.getItems().addAll(
+                    "Year 1 ", "Year 2", "Year 3", "Year 4"
+            );
 
-        comBoxSem.getItems().removeAll(comBoxSem.getItems());
-        comBoxSem.getItems().addAll(
-                "Semester 1", "Semester 2"
-        );
+            comBoxSem.getItems().removeAll(comBoxSem.getItems());
+            comBoxSem.setPromptText("Select");
+            comBoxSem.getItems().addAll(
+                    "Semester 1", "Semester 2"
+            );
 
-        comBoxIntake.getItems().removeAll(comBoxIntake.getItems());
-        comBoxIntake.getItems().addAll(
-                "Regular Intake", "June Intake"
-        );
+            comBoxIntake.getItems().removeAll(comBoxIntake.getItems());
+            comBoxIntake.getItems().addAll(
+                    "Regular Intake", "June Intake"
+            );
 
-        comBoxFac.getItems().removeAll(comBoxFac.getItems());
-        comBoxFac.getItems().addAll(
-                "IT","BM","ENG","H&S","ARCH","HOS","NUR"
-        );
+            comBoxFac.getItems().removeAll(comBoxFac.getItems());
+            comBoxFac.getItems().addAll(
+                    "IT","BM","ENG","H&S","ARCH","HOS","NUR"
+            );
 
 
-        comBoxCen.getItems().removeAll(comBoxCen.getItems());
-        comBoxCen.getItems().addAll(
-                "Malabe", "Kandy" , "Matara", "Jaffna" , "Metro"
-        );
+            comBoxCen.getItems().removeAll(comBoxCen.getItems());
+            comBoxCen.getItems().addAll(
+                    "Malabe", "Kandy" , "Matara", "Jaffna" , "Metro"
+            );
+
+
+    }else {
+
+            year = TimeTableGeneratorDesktopApp.StudentBatches.studentBatchesController.year;
+            semester = TimeTableGeneratorDesktopApp.StudentBatches.studentBatchesController.semester;
+            intake = TimeTableGeneratorDesktopApp.StudentBatches.studentBatchesController.intake;
+            faculty = TimeTableGeneratorDesktopApp.StudentBatches.studentBatchesController.faculty;
+            programme = TimeTableGeneratorDesktopApp.StudentBatches.studentBatchesController.programme;
+//          programme = TimeTableGeneratorDesktopApp.StudentBatches.studentBatchesController.programme;
+//          programme = TimeTableGeneratorDesktopApp.StudentBatches.studentBatchesController.programme;
+
+
+            comboBoxYear.getItems().removeAll(comboBoxYear.getItems());
+            comboBoxYear.setPromptText(TimeTableGeneratorDesktopApp.StudentBatches.studentBatchesController.year);
+            comboBoxYear.getItems().addAll(
+                    "Year 1 ", "Year 2", "Year 3", "Year 4"
+            );
+
+            comBoxSem.getItems().removeAll(comBoxSem.getItems());
+            comBoxSem.setPromptText(TimeTableGeneratorDesktopApp.StudentBatches.studentBatchesController.semester);
+            comBoxSem.getItems().addAll(
+                    "Semester 1", "Semester 2"
+            );
+
+            comBoxIntake.getItems().removeAll(comBoxIntake.getItems());
+            comBoxIntake.getItems().addAll(
+                    "Regular Intake", "June Intake"
+            );
+
+            comBoxFac.getItems().removeAll(comBoxFac.getItems());
+            comBoxFac.getItems().addAll(
+                    "IT","BM","ENG","H&S","ARCH","HOS","NUR"
+            );
+
+
+            comBoxCen.getItems().removeAll(comBoxCen.getItems());
+            comBoxCen.getItems().addAll(
+                    "Malabe", "Kandy" , "Matara", "Jaffna" , "Metro"
+            );
+        }
+
     }
+
 
 
 
     @FXML
     public void selectBatchYear(ActionEvent actionEvent){
-        String year = comboBoxYear.getSelectionModel().getSelectedItem().toString();
+        year = comboBoxYear.getSelectionModel().getSelectedItem().toString();
         System.out.println(year);
     }
       public void selectBatchSem(ActionEvent actionEvent){
-        String sem = comBoxSem.getSelectionModel().getSelectedItem().toString();
-        System.out.println(sem);
+        semester = comBoxSem.getSelectionModel().getSelectedItem().toString();
+        System.out.println(semester);
     }
       public void selectBatchIntake(ActionEvent actionEvent){
-        String intake = comBoxIntake.getSelectionModel().getSelectedItem().toString();
+        intake = comBoxIntake.getSelectionModel().getSelectedItem().toString();
         System.out.println(intake);
     }
-      public void selectBatchFac(ActionEvent actionEvent){
-        String faculty = comBoxFac.getSelectionModel().getSelectedItem().toString();
+
+    public void selectBatchCen(ActionEvent actionEvent){
+        center = comBoxCen.getSelectionModel().getSelectedItem().toString();
+        System.out.println(center);
+    }
+
+    public void selectBatchFac(ActionEvent actionEvent){
+         faculty = comBoxFac.getSelectionModel().getSelectedItem().toString();
         System.out.println(faculty);
 
         if(faculty == "IT"){
@@ -104,18 +179,58 @@ public class BatchFormController implements Initializable {
         }
 
 
-
-    }
-      public void selectBatchCen(ActionEvent actionEvent){
-        String center = comBoxCen.getSelectionModel().getSelectedItem().toString();
-        System.out.println(center);
     }
 
+    public void selectBatchPro(ActionEvent actionEvent){
+        programme = comBoxPro.getSelectionModel().getSelectedItem().toString();
+        System.out.println(programme);
+    }
+
+    public Connection getConnection(){
+        Connection conn;
+        try{
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/timetabledb", "root","root");
+            return conn;
+        }catch(Exception ex){
+            System.out.println("Error: " + ex.getMessage());
+            return null;
+        }
+    }
+
+    private void executeQuery(String query) {
+        Connection conn = getConnection();
+        Statement st;
+        try{
+            st = conn.createStatement();
+            st.executeUpdate(query);
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
 
 
-    public void submitForm(ActionEvent actionEvent){
+    public void BatchsubmitActionHandler(ActionEvent actionEvent){
+        insertRecord();
+        year = "";
+        semester = "";
+        intake = "";
+        faculty = "";
+        programme = "";
+        center = "";
+
+        System.out.println(tfNoOfStd.getText());
         Stage stage = (Stage) submitAddBatch.getScene().getWindow();
         stage.close();
+
+
+    }
+
+    public void insertRecord(){
+//        int NoOfStudents = Integer.parseInt(tfNoOfStd.getText().toString());
+        String query = "INSERT INTO studentbatches (year,semester,intake,faculty,programme,center,noofstd) " +
+                "VALUES ('" +year+ "','" +semester+ "','" +intake+ "','" +faculty+ "','" +programme+ "','" +center+ "'," +tfNoOfStd.getText()+ ") ";
+        executeQuery(query);
+
     }
 
 
--- src/TimeTableGeneratorDesktopApp/StudentBatches/studentBatchesController.java
+++ src/TimeTableGeneratorDesktopApp/StudentBatches/studentBatchesController.java
@@ -1,31 +1,145 @@
 package TimeTableGeneratorDesktopApp.StudentBatches;
 
-import TimeTableGeneratorDesktopApp.FxmlLoader;
 import TimeTableGeneratorDesktopApp.Main;
-import com.sun.glass.ui.EventLoop;
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
+import javafx.stage.WindowEvent;
 
-import java.awt.*;
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ResourceBundle;
 
 public class studentBatchesController implements Initializable {
+    public static String year = "";
+    public static String semester = "";
+    public static String intake = "";
+    public static String faculty = "";
+    public static String programme = "";
+    public static String center = "";
+    public static int noofstd = 0;
+
+
     @FXML
     private Button addBathcBtn;
 
     @FXML
+    private Button refreshBatches;
+
+    @FXML
+    private TableView<StudentBatches> tvBatches;
+
+    @FXML
+    private TableColumn<StudentBatches, String> batColYear;
+
+
+    @FXML
+    private TableColumn<StudentBatches, String> batColIntake;
+
+    @FXML
+    private TableColumn<StudentBatches, Integer> batColNoOfStd;
+
+    @FXML
+    private TableColumn<StudentBatches, String> batColFac;
+
+    @FXML
+    private TableColumn<StudentBatches, String> batColPro;
+
+
+    @FXML
+    private TableColumn<StudentBatches, String> batColSem;
+
+
+    @FXML
+    private TableColumn<StudentBatches, String> batColCen;
+
+
+
+    @FXML
     private Pane studentsPane;
+
+
+    @FXML
+    void actionRefreshButton(ActionEvent actionEvent){
+        showBatches();
+    }
+
+
+    @FXML
+    void handleMouseAction(MouseEvent event) {
+        StudentBatches batch = tvBatches.getSelectionModel().getSelectedItem();
+
+        year = batch.getYear();
+        semester = batch.getSemester();
+        intake = batch.getIntake();
+        faculty = batch.getFaculty();
+        programme = batch.getProgramme();
+        center = batch.getCenter();
+        noofstd = batch.getNoofstd();
+
+
+
+        try{
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("BatchForm/batchForm.fxml"));
+            Parent root1 = (Parent) fxmlLoader.load();
+
+            Stage stage = new Stage();
+            stage.setTitle("Edit batch Details");
+            stage.initModality(Modality.WINDOW_MODAL);
+            stage.initOwner(studentsPane.getScene().getWindow());
+            stage.setResizable(false);
+            stage.setScene(new Scene(root1));
+            stage.show();
+
+
+            stage.setOnHidden(new EventHandler<WindowEvent>() {
+                @Override
+                public void handle(WindowEvent windowEvent) {
+                    Platform.runLater(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            year = "";
+                            semester = "";
+                            intake = "";
+                            faculty = "";
+                            programme = "";
+                            center = "";
+                        }
+                    });
+                }
+            });
+
+
+        }catch (Exception e){
+            System.out.println("can't load new window");
+        }
+
+
+
+    }
+
+
+
 
     @FXML
     void ActionEventAddBatchBtn(ActionEvent event) {
@@ -44,6 +158,7 @@ public class studentBatchesController implements Initializable {
 
         }catch (Exception e){
             System.out.println("can't load new window");
+            e.printStackTrace();
         }
     }
 
@@ -61,6 +176,75 @@ public class studentBatchesController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        showBatches();
+    }
+
+    public void showBatches() {
+        ObservableList<StudentBatches> list = getBatchesList();
+
+        batColYear.setCellValueFactory(new PropertyValueFactory<StudentBatches, String>("year"));
+        batColSem.setCellValueFactory(new PropertyValueFactory<StudentBatches, String>("semester"));
+        batColIntake.setCellValueFactory(new PropertyValueFactory<StudentBatches, String>("intake"));
+        batColFac.setCellValueFactory(new PropertyValueFactory<StudentBatches, String>("faculty"));
+        batColPro.setCellValueFactory(new PropertyValueFactory<StudentBatches, String>("programme"));
+        batColCen.setCellValueFactory(new PropertyValueFactory<StudentBatches, String>("center"));
+        batColNoOfStd.setCellValueFactory(new PropertyValueFactory<StudentBatches, Integer>("noofstd"));
+
+
+        tvBatches.setItems(list);
+
 
     }
+
+    public Connection getConnection(){
+        Connection conn;
+        try{
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/timetabledb", "root","root");
+            return conn;
+        }catch(Exception ex){
+            System.out.println("Error: " + ex.getMessage());
+            return null;
+        }
+    }
+
+    private void executeQuery(String query) {
+        Connection conn = getConnection();
+        Statement st;
+        try{
+            st = conn.createStatement();
+            st.executeUpdate(query);
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+
+    public ObservableList<StudentBatches> getBatchesList() {
+        ObservableList<StudentBatches> studentBatchesList = FXCollections.observableArrayList();
+        Connection conn = getConnection();
+        String query = "SELECT * FROM studentBatches";
+        Statement st;
+        ResultSet rs;
+
+        try {
+            st = conn.createStatement();
+            rs = st.executeQuery(query);
+            StudentBatches studentBatch;
+            while (rs.next()) {
+                studentBatch = new StudentBatches(rs.getInt("id"), rs.getString("year"), rs.getString("semester"),
+                        rs.getString("intake"), rs.getString("faculty"),
+                        rs.getString("programme"),
+                        rs.getString("center"),
+                        rs.getInt("noofstd"), rs.getString("batchID"));
+                studentBatchesList.add(studentBatch);
+            }
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return studentBatchesList;
+    }
+
+
+
+
 }
