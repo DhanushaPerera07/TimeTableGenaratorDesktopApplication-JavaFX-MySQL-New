ff81667398c852bef4c82fe748476fa24aeb6d22
DhanushaPerera07 <buddhika.dhanusha@gmail.com>
2020-08-13 12:09:40 +0530

    Commit 8 - by Dhanusha
    * Worked on addFacultyPopUp.fxml UI..
    * addFacultyPopUp.fxml is done

--- src/TimeTableGeneratorDesktopApp/FacultyDepartments/FacultyPopUps/AddFacultyPopUpController.java
+++ src/TimeTableGeneratorDesktopApp/FacultyDepartments/FacultyPopUps/AddFacultyPopUpController.java
@@ -1,5 +1,6 @@
 package TimeTableGeneratorDesktopApp.FacultyDepartments.FacultyPopUps;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 
 import java.net.URL;
@@ -9,5 +10,10 @@ public class AddFacultyPopUpController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+    }
+
+    public void ActionEventAddFacultyPopUp(ActionEvent actionEvent) {
+        // pop up - add a faculty action event on ADD BUTTON
+        System.out.println("clicked pop up - add a faculty action event on ADD BUTTON");
     }
 }
