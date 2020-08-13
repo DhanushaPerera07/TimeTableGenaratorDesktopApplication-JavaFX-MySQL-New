fecda01122e4fcf8a463592269d910d3619ea2ef
DhanushaPerera07 <buddhika.dhanusha@gmail.com>
2020-08-13 17:38:49 +0530

    Commit 11 - by Dhanusha
    * Department folder is taken back one directory.
    * Worked on deleting a faculty. Just the alert box.
    * Deleting a faculty confirmation alert box is done.

---
+++ src/TimeTableGeneratorDesktopApp/Departments/DepartmentsItem/DepartmentItemController.java
@@ -0,0 +1,4 @@
+package TimeTableGeneratorDesktopApp.Departments.DepartmentsItem;
+
+public class DepartmentItemController {
+}
--- src/TimeTableGeneratorDesktopApp/Departments/DepartmentsPopUps/AddDepartmentPopUpController.java
+++ src/TimeTableGeneratorDesktopApp/Departments/DepartmentsPopUps/AddDepartmentPopUpController.java
@@ -1,11 +1,11 @@
-package TimeTableGeneratorDesktopApp.FacultyDepartments.Departments;
+package TimeTableGeneratorDesktopApp.Departments.DepartmentsPopUps;
 
 import javafx.fxml.Initializable;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class DepartmentsController implements Initializable {
+public class AddDepartmentPopUpController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
