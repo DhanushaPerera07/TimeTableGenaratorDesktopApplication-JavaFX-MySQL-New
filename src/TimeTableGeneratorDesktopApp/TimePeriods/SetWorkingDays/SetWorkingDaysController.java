487d98f53a2a97835681ac081f57aa8d2c3fae8f
janakachinthana <janakachinthana1@gmail.com>
2020-08-14 22:52:21 +0530

    working days adding crud done

--- src/TimeTableGeneratorDesktopApp/TimePeriods/SetWorkingDays/SetWorkingDaysController.java
+++ src/TimeTableGeneratorDesktopApp/TimePeriods/SetWorkingDays/SetWorkingDaysController.java
@@ -1,13 +1,98 @@
 package TimeTableGeneratorDesktopApp.TimePeriods.SetWorkingDays;
 
+import TimeTableGeneratorDesktopApp.StudentBatches.StudentBatches;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ResourceBundle;
 
 public class SetWorkingDaysController implements Initializable {
+
+    public static int noDays;
+    public static int noDaysDB;
+
+    @FXML
+    private ComboBox<String> comboSelectDays;
+
+    @FXML
+    private Button populateBtn;
+
+    @FXML
+    private Label labelnoDays;
+
+
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        getWorkingDays();
+        labelnoDays.setText(String.valueOf("No of working days : " +noDaysDB));
 
     }
+
+    public Connection getConnection(){
+        Connection conn;
+        try{
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/timetabledb", "root","root");
+            System.out.println("Database connected");
+            return conn;
+        }catch(Exception ex){
+            System.out.println("Error: " + ex.getMessage());
+            return null;
+        }
+    }
+
+    private void executeQuery(String query) {
+        Connection conn = getConnection();
+        Statement st;
+        try {
+            st = conn.createStatement();
+            st.executeUpdate(query);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+
+    public void selectWorkingDays(ActionEvent actionEvent) {
+        noDays = comboSelectDays.getSelectionModel().getSelectedIndex() + 1;
+        System.out.println(noDays);
+    }
+
+    @FXML
+    public void insertRecord(ActionEvent actionEvent){
+        String query = "INSERT INTO nodays (noDays) " +
+                "VALUES (" +noDays +") ";
+        executeQuery(query);
+    }
+
+    public void getWorkingDays() {
+        Connection conn = getConnection();
+        String query = "SELECT * FROM nodays WHERE idno = 1";
+        Statement st;
+        ResultSet rs;
+        try {
+            st = conn.createStatement();
+            rs = st.executeQuery(query);
+
+            while (rs.next()) {
+                 noDaysDB= rs.getInt("noDays");
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+    }
+
+
 }
