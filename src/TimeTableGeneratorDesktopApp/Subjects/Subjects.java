d6f6454ae9b3d5377b8b76fbb4bcb69cd43ac03d
MenuraDewalegama <61499065+MenuraDewalegama@users.noreply.github.com>
2020-08-20 07:54:51 +0530

    Modules function completed

--- src/TimeTableGeneratorDesktopApp/Subjects/Subjects.java
+++ src/TimeTableGeneratorDesktopApp/Subjects/Subjects.java
@@ -1,4 +1,62 @@
 package TimeTableGeneratorDesktopApp.Subjects;
 
 public class Subjects {
+
+    private int idmodule;
+    private String moduleName;
+    private String moduleCode;
+    private String offeredYear;
+    private String offeredSemester;
+    private int lecHour;
+    private int tuteHour;
+    private int labHour;
+    private int evaluationHour;
+
+    public Subjects(int idmodule, String moduleName, String moduleCode, String offeredYear, String offeredSemester, int lecHour, int tuteHour, int labHour, int evaluationHour) {
+        this.idmodule = idmodule;
+        this.moduleName = moduleName;
+        this.moduleCode = moduleCode;
+        this.offeredYear = offeredYear;
+        this.offeredSemester = offeredSemester;
+        this.lecHour = lecHour;
+        this.tuteHour = tuteHour;
+        this.labHour = labHour;
+        this.evaluationHour = evaluationHour;
+    }
+
+    public int getIdmodule() {
+        return idmodule;
+    }
+
+    public String getModuleName() {
+        return moduleName;
+    }
+
+    public String getModuleCode() {
+        return moduleCode;
+    }
+
+    public String getOfferedYear() {
+        return offeredYear;
+    }
+
+    public String getOfferedSemester() {
+        return offeredSemester;
+    }
+
+    public int getLecHour() {
+        return lecHour;
+    }
+
+    public int getTuteHour() {
+        return tuteHour;
+    }
+
+    public int getLabHour() {
+        return labHour;
+    }
+
+    public int getEvaluationHour() {
+        return evaluationHour;
+    }
 }
