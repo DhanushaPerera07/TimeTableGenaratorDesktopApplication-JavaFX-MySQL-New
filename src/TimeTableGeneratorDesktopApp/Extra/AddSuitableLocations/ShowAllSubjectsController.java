254caf5ea69515f4d86a629e197307b347e0bed5
DhanushaPerera07 <buddhika.dhanusha@gmail.com>
2020-09-15 14:55:09 +0530

    Commit 39 - by Dhanusha
    * Out folder deleted
    * Working on sprint 2 (add suitable rooms for student group)
    - working on populate student batches list.

---
+++ src/TimeTableGeneratorDesktopApp/Extra/AddSuitableLocations/ShowAllSubjectsController.java
@@ -0,0 +1,15 @@
+package TimeTableGeneratorDesktopApp.Extra.AddSuitableLocations;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ShowAllSubjectsController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+
+}
--- src/TimeTableGeneratorDesktopApp/Extra/ExtraController.java
+++ src/TimeTableGeneratorDesktopApp/Extra/ExtraController.java
@@ -1,17 +1,24 @@
 package TimeTableGeneratorDesktopApp.Extra;
 
 import TimeTableGeneratorDesktopApp.FxmlLoader;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.TilePane;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ExtraController implements Initializable {
+
 
     @FXML
     private Button NASessionBtn;
@@ -20,7 +27,13 @@ public class ExtraController implements Initializable {
     private Button backBtn;
 
     @FXML
+    private Button btnAddSuitableLocation;
+
+    @FXML
     private Pane pane;
+
+    @FXML
+    private TilePane addSuitableRooms;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -33,10 +46,36 @@ public class ExtraController implements Initializable {
         FxmlLoader object = new FxmlLoader();
         Pane view = object.getPane("Extra/NotAvailableTime/NotAvailableTime");
         pane.getChildren().add(view);
-        
+
     }
 
 
+    @FXML
+    void openPopUpAddSuitableLocations(javafx.event.ActionEvent actionEvent) {
+
+        System.out.println("clicked Add suitable button");
+        FxmlLoader object = new FxmlLoader();
+        //Pane view = object.getPane("Extra/NotAvailableTime/NotAvailableTime");
+        Pane view = object.getPane("Extra/AddSuitableLocations/addSuitableLocation");
+        //System.out.println(pane.getChildren());
+        pane.getChildren().add(view);
+
+    }
+
+
+
+
+    @FXML
+    void openOnClickTilePopUpAddSuitableLocations(MouseEvent mouseEvent) {
+
+        System.out.println("clicked Add suitable button");
+        FxmlLoader object = new FxmlLoader();
+        //Pane view = object.getPane("Extra/NotAvailableTime/NotAvailableTime");
+        Pane view = object.getPane("Extra/AddSuitableLocations/addSuitableLocation");
+        //System.out.println(pane.getChildren());
+        pane.getChildren().add(view);
+
+    }
 
 
 }
